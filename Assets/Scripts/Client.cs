09bc26aa099ff8c63a6655c3394ab3420a16dc44
Tom Najdek <tom@doppnet.com>
2014-07-08 01:55:07 +0100

    yay I can spawn!
    networking 101

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class Client : MonoBehaviour {
+	public bool playing = false;
+	public string username = "Unnamed Guest";
 
 	public static void StartClient() {
 		Network.Connect("127.0.0.1", 25000);
@@ -10,5 +12,23 @@ public class Client : MonoBehaviour {
 
 	void OnConnectedToServer() {
 		Debug.Log("Connected to server");
+	}
+
+	void OnGUI() {
+		if (Network.isClient && !playing) {
+			username = GUI.TextField(new Rect(
+				Screen.width/2 - 50,
+				Screen.height/2 - 70,
+				100, 20), username
+			);
+
+			if(GUI.Button(new Rect(Screen.width/2 - 50, Screen.height/2 - 30, 100, 20), "Spawn")) {
+				networkView.RPC("SpawnPlayer", RPCMode.Server, username);
+			}
+
+			if(GUI.Button(new Rect(Screen.width/2 - 50, Screen.height/2 + 10, 100, 20), "Disconnect")) {
+				
+			}
+		}
 	}
 }
--- Assets/Scripts/Server.cs
+++ Assets/Scripts/Server.cs
@@ -1,9 +1,15 @@
-﻿#if INCLUDE_SERVER
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class Server : MonoBehaviour {
+	#if INCLUDE_SERVER
 	private int playerCount = 0;
+	private NetworkPlayer[] players;
+	private Object playerPrefab;
+
+	void Start() {
+		playerPrefab = Resources.Load("robot-player"); //GameObject.Find("robot-player");
+	}
 
 	public static void StartServer() {
 		//Network.incomingPassword = "";
@@ -18,6 +24,10 @@ public class Server : MonoBehaviour {
 
 	void OnPlayerConnected(NetworkPlayer player) {
 		Debug.Log("Player " + playerCount++ + " connected from " + player.ipAddress + ":" + player.port);
+
 	}
+	[RPC] void SpawnPlayer(string username, NetworkMessageInfo info) {
+		Network.Instantiate(playerPrefab, new Vector3(0f, 0f, 0f), Quaternion.identity, 0);
+	}
+	#endif
 }
-#endif
