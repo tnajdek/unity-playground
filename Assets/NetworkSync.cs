29871e112516f9a4f6c22a569a3878d20b11d77d
Tom Najdek <tom@doppnet.com>
2014-08-02 16:27:56 +0100

    Interpolation. Sweet! :cake:

--- Assets/NetworkSync.cs
+++ Assets/NetworkSync.cs
@@ -3,34 +3,47 @@ using System.Collections;
 
 public class NetworkSync : MonoBehaviour {
 	private Animator anim;
+	private Interpolate interpolate;
 
 	void Awake() {
 		anim = GetComponent<Animator>();
+		interpolate = GetComponent<Interpolate>();
 	}
 
 	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info) {
 		float animatorSpeed = 0f;
 		float animatorAngularSpeed = 0f;
 		Vector3 playerColor = new Vector3(Color.black.r, Color.black.g, Color.black.b);
+		Vector3 playerPosition = Vector3.zero;
+		Quaternion playerRotation = Quaternion.identity;
 
 		if (stream.isWriting) {
 			animatorSpeed = anim.GetFloat("Speed");;
 			animatorAngularSpeed = anim.GetFloat("AngularSpeed");
 			Color pColor = transform.Find("Light").GetComponent<Light>().color;
 			playerColor = new Vector3(pColor.r, pColor.g, pColor.b);
+			playerPosition = transform.position;
+			playerRotation = transform.rotation;
 
 			stream.Serialize(ref animatorSpeed);
 			stream.Serialize(ref animatorAngularSpeed);
 			stream.Serialize(ref playerColor);
+			stream.Serialize(ref playerPosition);
+			stream.Serialize(ref playerRotation);
 		} else {
 			stream.Serialize(ref animatorSpeed);
 			stream.Serialize(ref animatorAngularSpeed);
 			stream.Serialize(ref playerColor);
+			stream.Serialize(ref playerPosition);
+			stream.Serialize(ref playerRotation);
 
 			anim.SetFloat("Speed", animatorSpeed);
 			anim.SetFloat("AngularSpeed", animatorAngularSpeed);
 			Color pColor = new Color(playerColor.x, playerColor.y, playerColor.z);
 			transform.Find("Light").GetComponent<Light>().color = pColor;
+			//transform.position = playerPosition;
+			//transform.rotation = playerRotation;
+			interpolate.AddPacket(info.timestamp, playerPosition, playerRotation);
 		}
 	}
 }
--- Assets/Scripts/Interpolate.cs
+++ Assets/Scripts/Interpolate.cs
@@ -1,15 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public struct Packet {
+	public Vector3 position;
+	public Quaternion rotation;
+	public double timestamp;
+}
+
 public class Interpolate : MonoBehaviour {
+	private Packet latestFrame;
+	private Packet lastAppliedFrame;
+	private double progress = 0;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		if(latestFrame.timestamp != 0 && lastAppliedFrame.timestamp != 0) {
+			double diff = latestFrame.timestamp - lastAppliedFrame.timestamp;
+			diff = progress/diff;
+			transform.position = Vector3.Lerp(lastAppliedFrame.position, latestFrame.position, (float)diff);
+			transform.rotation = Quaternion.Slerp(lastAppliedFrame.rotation, latestFrame.rotation, (float)diff);
+			progress += Time.deltaTime;
+		} else if(latestFrame.timestamp != 0) {
+			transform.position = latestFrame.position;
+			transform.rotation = latestFrame.rotation;
+		}
+	}
+
+	public void AddPacket(Packet packet) {
+		lastAppliedFrame = latestFrame;
+		latestFrame = packet;
+		progress = 0;
+
+	}
+
+	public void AddPacket(double timestamp, Vector3 pos, Quaternion rotation) {
+		Packet newPacket = new Packet();
+		newPacket.position = pos;
+		newPacket.rotation = rotation;
+		newPacket.timestamp = timestamp;
+		AddPacket(newPacket);
 	}
 }
